aabb331efc23b451004538945310f7425173bfc7
cerveau08 <malickcoly342@gmail.com>
2020-09-03 19:53:59 +0000

    login and signUp ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:login_signup/constant.dart';
+import 'package:login_signup/screens/welcom/welcom_screen.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Auth',
       theme: ThemeData(
-      
-        primarySwatch: Colors.blue,
-        
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      
+      home: WelcomeScreen(),
     );
   }
 }
